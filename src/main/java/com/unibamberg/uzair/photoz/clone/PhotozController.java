4def142dd1478ead27a02fcfc09f63c43ec546e2
Ghazanfar Shah Uzair <59178655+gsuzair@users.noreply.github.com>
2024-11-25 13:50:26 +0100

    Initial commit

---
+++ src/main/java/com/unibamberg/uzair/photoz/clone/PhotozController.java
@@ -0,0 +1,13 @@
+package com.unibamberg.uzair.photoz.clone;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PhotozController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello World";
+    }
+}
