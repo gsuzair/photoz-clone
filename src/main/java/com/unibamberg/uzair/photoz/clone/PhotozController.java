04f9a34cd1a452166eec1d21a3fc4a0d8186fee7
Ghazanfar Shah Uzair <59178655+gsuzair@users.noreply.github.com>
2024-11-29 09:29:34 +0100

    post mapping

--- src/main/java/com/unibamberg/uzair/photoz/clone/PhotozController.java
+++ src/main/java/com/unibamberg/uzair/photoz/clone/PhotozController.java
@@ -1,15 +1,13 @@
 package com.unibamberg.uzair.photoz.clone;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 public class PhotozController {
@@ -26,6 +24,13 @@ public class PhotozController {
     @GetMapping("/photoz")
     public Collection<Photo> get(){
         return db.values();
+    }
+
+    @PostMapping("/photoz")
+    public Photo create(@RequestBody Photo photo){
+        photo.setId(UUID.randomUUID().toString());
+        db.put(photo.getId(), photo);
+        return photo;
     }
 
     @GetMapping("/photoz/{id}")
