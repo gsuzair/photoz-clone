e98cd906b8c12ccb9e40f454bccd7a1cdb0a2954
Ghazanfar Shah Uzair <59178655+gsuzair@users.noreply.github.com>
2024-11-30 19:17:15 +0100

    validations and file upload

--- src/main/java/com/unibamberg/uzair/photoz/clone/Photo.java
+++ src/main/java/com/unibamberg/uzair/photoz/clone/Photo.java
@@ -1,10 +1,23 @@
 package com.unibamberg.uzair.photoz.clone;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public class Photo {
     private String id;
+
+    @NotEmpty
     private String fileName;
 
-    public Photo() {
+    private byte[] data;
+
+    public Photo() {}
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
     }
 
     public Photo(String id, String fileName){
--- src/main/java/com/unibamberg/uzair/photoz/clone/PhotozController.java
+++ src/main/java/com/unibamberg/uzair/photoz/clone/PhotozController.java
@@ -1,9 +1,12 @@
 package com.unibamberg.uzair.photoz.clone;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -27,8 +30,11 @@ public class PhotozController {
     }
 
     @PostMapping("/photoz")
-    public Photo create(@RequestBody Photo photo){
+    public Photo create(@RequestPart("data") MultipartFile file) throws IOException {
+        Photo photo = new Photo();
         photo.setId(UUID.randomUUID().toString());
+        photo.setFileName(file.getOriginalFilename());
+        photo.setData(file.getBytes());
         db.put(photo.getId(), photo);
         return photo;
     }
